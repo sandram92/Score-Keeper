b0949c5607bdd6e8efcd1b0422b95d918b6ff991
Sandra Margol <sandramargol@Sandras-MacBook-Air.local>
2022-06-27 22:49:53 +0100

    added classList to display one and two

--- app.js
+++ app.js
@@ -11,20 +11,29 @@ let playerTwo = 0
 let isGameOver = false
 
 buttonOne.addEventListener('click', function(){
-
+    if(playerTwo !== winningScore) {
     if(winningScore !== playerOne){
       playerOne +=1
-    
       displayOne.textContent = playerOne
-    }   
+     if(playerOne === winningScore){
+         displayOne.classList.add('green')
+         displayTwo.classList.add('red')
+     }
+    }  
+}
 })
 
 buttonTwo.addEventListener('click', function () {
-    
+    if(playerOne !== winningScore) {
     if(winningScore !== playerTwo){
     playerTwo +=1
     displayTwo.textContent = playerTwo
+    if(playerTwo=== winningScore){
+        displayTwo.classList.add('green')
+        displayOne.classList.add('red')
     }
+    }
+}
 })
 
 reset.addEventListener('click', function () {
@@ -32,6 +41,8 @@ reset.addEventListener('click', function () {
     playerTwo=0
     displayOne.textContent = 0
     displayTwo.textContent = 0
+    displayOne.classList.remove('green', 'red')
+    displayTwo.classList.remove('green', 'red')
 
 })
 
@@ -39,3 +50,4 @@ gameRound.addEventListener('change', function (){
     const value =parseInt(gameRound.options[gameRound.selectedIndex].value);
     winningScore=value
 })
+
