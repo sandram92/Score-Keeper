5bf987e002354dd80f2576d9d5f98045c52001e4
Sandra Margol <sandramargol@Sandras-MacBook-Air.local>
2022-06-27 22:27:54 +0100

    added Event Listener to select options

--- app.js
+++ app.js
@@ -1,22 +1,41 @@
 const buttonOne = document.querySelector('.playerOne')
 const buttonTwo = document.querySelector('.playerTwo')
-
 const displayOne = document.querySelector('.displayOne')
 const displayTwo = document.querySelector('.displayTwo')
+const reset = document.querySelector('.reset')
+const gameRound = document.querySelector('#gameRounds')
 
-
-
-let winningScore = 5
+let winningScore = 8
 let playerOne = 0
 let playerTwo = 0
+let isGameOver = false
 
 buttonOne.addEventListener('click', function(){
+
+    if(winningScore !== playerOne){
+      playerOne +=1
     
-    playerOne +=1
-    displayOne.textContent = playerOne
+      displayOne.textContent = playerOne
+    }   
 })
 
 buttonTwo.addEventListener('click', function () {
+    
+    if(winningScore !== playerTwo){
     playerTwo +=1
     displayTwo.textContent = playerTwo
+    }
+})
+
+reset.addEventListener('click', function () {
+    playerOne=0
+    playerTwo=0
+    displayOne.textContent = 0
+    displayTwo.textContent = 0
+
+})
+
+gameRound.addEventListener('change', function (){
+    const value =parseInt(gameRound.options[gameRound.selectedIndex].value);
+    winningScore=value
 })
