c4085832a98c8371a8232ce3e0297895d4cbe11b
Sandra Margol <sandramargol@Sandras-MacBook-Air.local>
2022-06-29 20:39:36 +0100

    refactoring the code

--- app.js
+++ app.js
@@ -1,53 +1,55 @@
-const buttonOne = document.querySelector('.playerOne')
-const buttonTwo = document.querySelector('.playerTwo')
-const displayOne = document.querySelector('.displayOne')
-const displayTwo = document.querySelector('.displayTwo')
-const reset = document.querySelector('.reset')
+const p1 = {
+    score:0,
+    button: document.querySelector('.playerOne'),
+    display: document.querySelector('.displayOne')
+}
+
+const p2 ={
+    score:0,
+    button: document.querySelector('.playerTwo'),
+    display: document.querySelector('.displayTwo')
+}
+
+const resetBt = document.querySelector('.reset')
 const gameRound = document.querySelector('#gameRounds')
 
 let winningScore = 8
-let playerOne = 0
-let playerTwo = 0
 let isGameOver = false
 
-buttonOne.addEventListener('click', function(){
-    if(playerTwo !== winningScore) {
-    if(winningScore !== playerOne){
-      playerOne +=1
-      displayOne.textContent = playerOne
-     if(playerOne === winningScore){
-         displayOne.classList.add('green')
-         displayTwo.classList.add('red')
-     }
-    }  
+function updateScore(player,opponent){
+      if(!isGameOver){
+          player.score += 1;
+          if(player.score === winningScore){
+              isGameOver = true
+              player.display.classList.add('green')
+              opponent.display.classList.add('red')   
+          }
+          player.display.textContent = player.score
+      }
 }
+
+p1.button.addEventListener('click', function(){
+    updateScore(p1,p2)
+    
 })
 
-buttonTwo.addEventListener('click', function () {
-    if(playerOne !== winningScore) {
-    if(winningScore !== playerTwo){
-    playerTwo +=1
-    displayTwo.textContent = playerTwo
-    if(playerTwo=== winningScore){
-        displayTwo.classList.add('green')
-        displayOne.classList.add('red')
-    }
-    }
+p2.button.addEventListener('click', function () {
+    updateScore(p2,p1)
+})
+
+function resetScores (){
+     isGameOver = false
+     for(let p of [p1,p2]){
+        p.score=0
+        p.display.textContent = 0
+        p.display.classList.remove('green', 'red')
+     }    
 }
-})
-
-reset.addEventListener('click', function () {
-    playerOne=0
-    playerTwo=0
-    displayOne.textContent = 0
-    displayTwo.textContent = 0
-    displayOne.classList.remove('green', 'red')
-    displayTwo.classList.remove('green', 'red')
-
-})
 
 gameRound.addEventListener('change', function (){
-    const value =parseInt(gameRound.options[gameRound.selectedIndex].value);
+    // const value =parseInt(gameRound.options[gameRound.selectedIndex].value);
+    let value = parseInt(this.value)
     winningScore=value
+    resetScores()
 })
-
+resetBt.addEventListener('click', resetScores)
